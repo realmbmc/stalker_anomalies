63c1d90c79c01ad06441d8cfe73b51b51af656ad
REALM <realm_bmc@hotmail.com>
2018-08-07 21:40:30 +0200

    fix for strings

--- mod/diwako_anomalies/cfgMagazines.hpp
+++ mod/diwako_anomalies/cfgMagazines.hpp
@@ -6,9 +6,9 @@ class cfgMagazines {
 		scope = 2;
 		count = 20;
 		mass = 30;
-		descriptionShort = "STR_bolts_infinite_descr";
-		displayName = "STR_bolts_infinite_name";
-		displayNameShort = "STR_bolts_infinite_name";
+		descriptionShort = "$STR_bolts_infinite_descr";
+		displayName = "$STR_bolts_infinite_name";
+		displayNameShort = "$STR_bolts_infinite_name";
 		model = "\A3\weapons_F\ammo\mag_univ.p3d";
 		/*picture = "\diwako_anomalies\data\ui\bolts.paa";*/
 		picture = "\A3\Weapons_f\data\UI\gear_satchel_CA.paa";
@@ -18,9 +18,9 @@ class cfgMagazines {
 	class bolts_one_mag : bolts_infinite_mag {
 		scope = 2;
 		mass = 1;
-		displayName = "STR_bolts_one_name";
-		displayNameShort = "STR_bolts_one_name";
-		descriptionShort = "STR_bolts_one_descr";
+		displayName = "$STR_bolts_one_name";
+		displayNameShort = "$STR_bolts_one_name";
+		descriptionShort = "$STR_bolts_one_descr";
 		picture = "\diwako_anomalies\data\ui\bolts.paa";
 		model = "\diwako_anomalies\data\models\bolt.p3d";
 		count = 1;
